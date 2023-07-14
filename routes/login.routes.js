df4ff33fbb0b4064df81c26741fed3a03667f5e6
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-07-14 18:20:03 +0200

    fix page routing

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -3,24 +3,20 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const { isLoggedOut } = require("../middlewares/secure-routes.middlewear");
 
-router.get("/login", (req, res) => {
+router.get("/login", isLoggedOut, (req, res) => {
   res.render("authFolder/login");
 });
 
-router.post("/login", async (req, res) => {
+router.post("/login", isLoggedOut, async (req, res) => {
   const userLogin = req.body;
   try {
     const checkedUser = await User.findOne({ username: userLogin.username });
-    console.log("print checkedUser :", checkedUser.password);
-    console.log("print passwordHash :", checkedUser.passwordHash);
-    console.log("print userLogin :", userLogin.password);
     if (checkedUser) {
       if (bcrypt.compareSync(userLogin.password, checkedUser.passwordHash)) {
         const loggedUser = { ...checkedUser._doc };
         delete loggedUser.passwordHash;
         req.session.currentUser = loggedUser;
-        // console.log(req.session);
-        res.redirect("/profile/" + checkedUser.userId);
+        res.redirect("/profile");
       } else {
         console.log("Incorect password or username");
         res.render("authFolder/login", {
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -8,29 +8,21 @@ const {
   isLoggedOut,
 } = require("../middlewares/secure-routes.middlewear");
 
-router.get("/profile/:id", (req, res, next) => {
-  const id = req.params.id;
-  console.log("GET profile: ", id);
-  res.render(
-    "profile",
-    { userId: id },
-    { userInSession: req.session.currentUser }
-  );
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
 });
 
 //Create book
-router.get("/create-book/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("GET create-book: ", id);
-  res.render("create-book", { userId: id });
+router.get("/create-book", (req, res) => {
+  res.render("create-book");
 });
 
 //creating book
 router.post("/create-book", async (req, res) => {
-  console.log(" book req body: ", req.body);
-  const userId = req.body.userId;
-  const bookId = uuidv4();
+  console.log(" book req body: ", req.session);
+  const userId = req.session.currentUser.userId;
   console.log("POST create-book: ", userId);
+  const bookId = uuidv4();
   try {
     const book = await Book.create({ ...req.body, userId, bookId });
     console.log("Created Book: ", book);
