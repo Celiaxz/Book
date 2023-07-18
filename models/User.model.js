c6d4d8c55a897164ecfbc87c8e172306bf117c31
3anza <salmaaldubouni@gmail.com>
2023-07-18 21:11:03 +0200

    almost done

--- models/User.model.js
+++ models/User.model.js
@@ -18,6 +18,8 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    image: String
+    ,
     savedBooks: {
       type: [String],
       default: [],
--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -3,25 +3,39 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const { v4: uuidv4 } = require("uuid");
 const { isLoggedOut } = require("../middlewares/secure-routes.middlewear");
+const uploader = require('../config/cloudinary.config.js');
 
 router.get("/signup", isLoggedOut, (req, res, next) => {
   res.render("authFolder/signup");
 });
 
-router.post("/signup", isLoggedOut, async (req, res) => {
+const defaultProfileImage = './images/LOGO.png';
+
+router.post("/signup", uploader.single("profileImage"), isLoggedOut, async (req, res) => {
   try {
     const userId = uuidv4();
     const data = { ...req.body, userId };
-    console.log("this is my data:", req.body);
+
+    if (req.file) {
+      const image = req.file.path;
+      data.image = image;
+    } else {
+      data.image = defaultProfileImage; // Assign the default profile image
+    }
+    console.log("This is my data:", data);
     delete data.password;
+
     const salt = bcrypt.genSaltSync(13);
     data.passwordHash = bcrypt.hashSync(req.body.password, salt);
-    // const data = { ...req.body };
+
     const newUser = await User.create(data);
-    console.log("my newUser:", newUser);
+    console.log("My newUser:", newUser);
+
     res.redirect("/login");
   } catch (err) {
     console.log(err);
+    res.status(500).json({ error: "Error signing up" });
   }
 });
+
 module.exports = router;
